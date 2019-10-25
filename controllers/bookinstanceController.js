2bb648303924d477a14dc39accd4d7a7f80672c1
Monkey Do <monkeyscapegoat@gmail.com>
2019-10-25 02:33:25 -0400

    work on book instance delete

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -97,7 +97,17 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+        if (err) { return next(err); }
+        if (bookinstance==null) { // No results.
+            res.redirect('/catalog/bookinstances')
+        }
+        // Successful, so render.
+        res.render('bookinstance_delete', { title: 'Delete Book Instance', bookinstance: bookinstance } );
+    });
 };
 
 // Handle BookInstance delete on POST.
